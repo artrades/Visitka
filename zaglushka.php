6069e997bbff8d1ac9a845ddc62774a78f889830
user <artrades_@mail.ru>
2023-09-13 12:57:33 +0300

    Настроен экспорт в БД

--- zaglushka.php
+++ zaglushka.php
@@ -2,4 +2,27 @@
 
 echo "Имя написавшего: " . $_POST['User_name'] . '<br>';
 echo "Почта написавшего: " . $_POST['User_email'] . '<br>';
-echo "Сообщение: " . $_POST['User_message'] . '<br>';
+echo "Сообщение: " . $_POST['User_message'] . '<br><hr>';
+
+$connection = mysqli_connect('localhost','root','','VizitkaData' );
+
+if ($connection == false){
+    echo "Не удалось подключиться к БД";
+    echo mysqli_connect_error();
+    exit();
+}else{
+    echo "Удалось подключиться к БД" . '<br><hr>';
+}
+
+$sql = "INSERT INTO `Messages` ( `Date_time`, `Visitor_name`, `Email`, `Message`) VALUES (now(), '" . $_POST['User_name'] . "', '" . $_POST['User_email'] . "', '" . $_POST['User_message'] . "')";
+
+echo $sql  . '<br><hr>';
+
+if($connection->query($sql)){
+    echo "Данные успешно добавлены";
+} else{
+    echo "Ошибка: " . $connection->error;
+}
+$connection->close();
+
+?>
