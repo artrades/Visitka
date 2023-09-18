d4bb89d76e28fab9b2702ee200af677485fa8a78
user <artrades_@mail.ru>
2023-09-18 13:29:46 +0300

    Экспорт обращений в телеграм

--- zaglushka.php
+++ zaglushka.php
@@ -19,10 +19,32 @@ $sql = "INSERT INTO `Messages` ( `Date_time`, `Visitor_name`, `Email`, `Message`
 echo $sql  . '<br><hr>';
 
 if($connection->query($sql)){
-    echo "Данные успешно добавлены";
+    echo "Данные успешно добавлены" . '<br><hr>';
 } else{
     echo "Ошибка: " . $connection->error;
 }
 $connection->close();
+
+//текст сообщения
+//"%0A" - это переход на новую строку  /   тэг <br> телеграм не переваривает
+$mes_text = "Имя: "."<b>".$_POST['User_name']."</b>"."%0A"."Почта: "."<b>".$_POST['User_email']."</b>"."%0A"."Сообщение: "."<b>".$_POST['User_message']."</b>";
+
+//https://api.telegram.org/bot******************************/getUpdates
+// после того как бот зарущен и добавлен - найти отрицательный чатайди(отрицательный значит групповой)
+
+$token = "****************************";
+$chatid = "**********";
+
+echo "Connect2tg"  . '<br><hr>';
+echo $mes_text  . '<br><hr>';
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chatid}&parse_mode=html&text={$mes_text}","r");
+
+if ($sendToTelegram)
+{
+    echo "Connected2tg"  . '<br><hr>';
+}else
+{
+    echo "NOTConnected2tg"  . '<br><hr>';
+};
 
 ?>
